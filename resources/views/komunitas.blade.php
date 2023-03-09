98546b1c454739dfc00191259d34b2c099827ba6
risa <risairma21@gmail.com>
2023-03-09 08:30:38 +0700

    refactor: update button comunity

--- resources/views/komunitas.blade.php
+++ resources/views/komunitas.blade.php
@@ -62,14 +62,16 @@
             <div class="row">
                 @foreach ($comunity as $comunities)
                     <div class="col-md-3 d-flex align-items-stretch">
-                        <div class="card card-3 card-button w-100" onclick="window.location='card.html'">
+                        <div class="card card-3 card-button w-100" onclick="">
                             <div class="card-body">
 
                                 <div class="card-content p-3">
                                     <span class="font-space-2 font-size-12 text-grey">{{ $comunities->platform }}</span>
                                     <h5 class="mt-1 font-weight-1"><b>{{ $comunities->name }}</b></h5>
                                     <div class="d-grid gap-2 my-3">
-                                        <button class="btn btn-success" type="button">Gabung Komunitas</button>
+                                        <button class="btn btn-success" type="button"
+                                            onclick="window.location.href='{{ $comunities->link }}'">Gabung
+                                            Komunitas</button>
                                     </div>
                                 </div>
                             </div>
